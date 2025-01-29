c710b8690faaa0a49f2363b3b8588c8ec54e866e
andy-635 <161386628+andy-635@users.noreply.github.com>
2025-01-29 20:01:07 +0000

    Commit 2

---
+++ src/MixMatch.java
@@ -0,0 +1,21 @@
+public class MixMatch{
+    public static void main (String[] args){
+        String s = "Hello";
+        char c = 'c';
+        short num1 = 1;
+        int num2 = 2;
+        long num3 = 3;
+        float num4 = 1.0f;
+        double num5 = 1.1; 
+        boolean notTrue = true;
+        System.out.println(s + " : prints the words or symols located in the string variable");
+        System.out.println(c + " : prints the character in the character c. different that a string since it can only hold 1 char value");
+        System.out.println(num1+ " : short can only hold small intager numbers");
+        System.out.println(num2+ " : int can hold a bigger number than short");
+        System.out.println(num3 + " : long can hold a larger number than an in ");
+        System.out.println(num4 +" : unlike the other, float can hold decimal numbers");
+        System.out.println(num5 + " : double can hold more decimal numbers than float");
+        System.out.println(notTrue + " ; boolean can only hold true or false values");
+
+    }
+}
